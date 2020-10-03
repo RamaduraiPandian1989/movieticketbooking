33624b6b54251d41f87bedf2cc6ce1a17967f3f9
Ramadurai Pandian <ramaduraipandian.s@gmail.com>
2020-10-03 10:11:23 +0530

    controller settings added

--- moviebooking/src/main/java/com/booking/moviebooking/controller/BookingController.java
+++ moviebooking/src/main/java/com/booking/moviebooking/controller/BookingController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Ramad
+ * 
+ * Program to book a ticket
  *
  */
 @RestController
 public class BookingController {
+	
 
 }
