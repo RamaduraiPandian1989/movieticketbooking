606287cb93c5e9cfe2c29b1ff3cc86ee2f027d6e
Ramadurai Pandian <ramaduraipandian.s@gmail.com>
2020-10-03 10:01:37 +0530

    initial changes

---
+++ moviebooking/src/main/java/com/booking/moviebooking/controller/BookingController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.booking.moviebooking.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ramad
+ *
+ */
+@RestController
+public class BookingController {
+
+}
